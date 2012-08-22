85c62554086ef1d5271adcaf57ec217b947fe765
Niels Abspoel <aboe76@gmail.com>
2012-08-22 07:23:36 +0200

    reformat init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,9 @@
 class chrony (
-        $ntpservers            = [ '0.nl.pool.ntp.org',
-                                   '1.nl.pool.ntp.org',
-                                   '2.nl.pool.ntp.org',
-                                   '3.nl.pool.ntp.org', ]
+  $ntpservers = [ 
+    '0.nl.pool.ntp.org',
+    '1.nl.pool.ntp.org',
+    '2.nl.pool.ntp.org',
+    '3.nl.pool.ntp.org', ]
 ){
     package { 'chrony':
         ensure => present,
@@ -12,15 +13,15 @@ class chrony (
         ensure   => present,
         owner    => root,
         group    => root,
-        mode     => 0644,
+        mode     => '0644',
         content  => template('chrony/chrony.conf.erb'),
         require  => Package['chrony'],
         notify   => Service['chrony.service'],
     }
 
     service { 'chrony.service':
-        provider   => systemd,
         ensure     => running,
+        provider   => systemd,
         hasstatus  => true,
         hasrestart => true,
         enable     => true,
