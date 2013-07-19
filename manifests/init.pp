a5bd5798d22ded9af15d3ed504501a8194135cf8
Niels Abspoel <aboe76@gmail.com>
2013-07-19 19:31:56 +0200

    update chrony module ready for forge

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,23 +13,16 @@ class chrony (
   $service_manage       = $chrony::params::service_manage,
   $service_name         = $chrony::params::service_name,) inherits
 chrony::params {
-  if $autoupdate {
-    notice('autoupdate parameter has been deprecated
-    and replaced with package_ensure.
-    Set this to latest for the same behavior as autoupdate => true.')
-  }
 
   include '::chrony::install'
   include '::chrony::config'
   include '::chrony::service'
 
-  # Anchor this as per #8140 - this ensures that classes won't float off and
-  # mess everything up.  You can read about this at:
-  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
   anchor { 'chrony::begin': }
 
   anchor { 'chrony::end': }
 
-  Anchor['chrony::begin'] -> Class['::chrony::install'] -> Class['::chrony::config'
-    ] ~> Class['::chrony::service'] -> Anchor['chrony::end']
+  Anchor['chrony::begin'] -> Class['::chrony::install'
+    ] -> Class['::chrony::config'] ~> Class['::chrony::service'
+    ] -> Anchor['chrony::end']
 }
