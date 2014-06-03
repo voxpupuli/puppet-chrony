27ddde4cc15063eb46b129b9539ec4e842e7fe3f
Ben Allen <bsallen@alcf.anl.gov>
2014-06-03 13:52:15 -0500

    Set redhat chrony params

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,11 +18,11 @@ class chrony::params {
     }
     'RedHat' : {
       $config = '/etc/chrony.conf'
-      $config_template = 'chrony/chrony.conf.archlinux.erb'
+      $config_template = 'chrony/chrony.conf.redhat.erb'
       $config_keys = 'etc/chrony.keys'
-      $config_keys_template = 'chrony/chrony.keys.archlinux.erb'
+      $config_keys_template = 'chrony/chrony.keys.redhat.erb'
       $package_name = ['chrony']
-      $service_name = 'chrony'
+      $service_name = 'chronyd'
       $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
     }
 
