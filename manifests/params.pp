b38c480fd2ec13ad925107c727cdbc257f52e219
Ben Allen <bsallen@alcf.anl.gov>
2014-06-02 23:56:53 -0500

    Add redhat params, match the redhat chrony.conf template match the RHEL7 RPM closer

--- manifests/params.pp
+++ manifests/params.pp
@@ -16,6 +16,15 @@ class chrony::params {
       $service_name = 'chrony'
       $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
     }
+    'RedHat' : {
+      $config = '/etc/chrony.conf'
+      $config_template = 'chrony/chrony.conf.archlinux.erb'
+      $config_keys = 'etc/chrony.keys'
+      $config_keys_template = 'chrony/chrony.keys.archlinux.erb'
+      $package_name = ['chrony']
+      $service_name = 'chrony'
+      $servers = ['0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org',]
+    }
 
     default     : {
       fail("The ${module_name} module is not supported
