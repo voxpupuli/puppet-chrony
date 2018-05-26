c7880f53dc0f3198a95e6f95b7707c38f8a6ef68
othalla <othalla.lf@gmail.com>
2018-05-26 20:53:23 +0200

    [Enhance] align equals signs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,45 +1,45 @@
 class chrony::params {
-  $commandkey       = 0
-  $keys             = []
-  $package_ensure   = 'present'
-  $refclocks        = []
-  $service_enable   = true
-  $service_ensure   = 'running'
-  $service_manage   = true
-  $chrony_password  = 'xyzzy'
-  $queryhosts       = []
-  $port             = 0
+  $commandkey         = 0
+  $keys               = []
+  $package_ensure     = 'present'
+  $refclocks          = []
+  $service_enable     = true
+  $service_ensure     = 'running'
+  $service_manage     = true
+  $chrony_password    = 'xyzzy'
+  $queryhosts         = []
+  $port               = 0
   $config_keys_manage = true
   $mailonchange       = undef
   $threshold          = 0.5
 
   case $::osfamily {
     'Archlinux' : {
-      $config = '/etc/chrony.conf'
-      $config_template = 'chrony/chrony.conf.archlinux.erb'
-      $config_keys = '/etc/chrony.keys'
+      $config            = '/etc/chrony.conf'
+      $config_template   = 'chrony/chrony.conf.archlinux.erb'
+      $config_keys       = '/etc/chrony.keys'
       $config_keys_owner = 0
       $config_keys_group = 0
       $config_keys_mode  = '0644'
-      $service_name = 'chrony'
+      $service_name      = 'chrony'
     }
     'RedHat' : {
-      $config = '/etc/chrony.conf'
-      $config_template = 'chrony/chrony.conf.redhat.erb'
-      $config_keys = '/etc/chrony.keys'
+      $config            = '/etc/chrony.conf'
+      $config_template   = 'chrony/chrony.conf.redhat.erb'
+      $config_keys       = '/etc/chrony.keys'
       $config_keys_owner = 0
       $config_keys_group = chrony
       $config_keys_mode  = '0640'
-      $service_name = 'chronyd'
+      $service_name      = 'chronyd'
     }
     'Debian' : {
-      $config = '/etc/chrony/chrony.conf'
-      $config_template = 'chrony/chrony.conf.debian.erb'
-      $config_keys = '/etc/chrony/chrony.keys'
+      $config            = '/etc/chrony/chrony.conf'
+      $config_template   = 'chrony/chrony.conf.debian.erb'
+      $config_keys       = '/etc/chrony/chrony.keys'
       $config_keys_owner = 0
       $config_keys_group = 0
       $config_keys_mode  = '0640'
-      $service_name = 'chrony'
+      $service_name      = 'chrony'
     }
 
     default     : {
