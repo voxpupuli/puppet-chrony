ae6cd687ddc0124dd215071af52d3fff871172df
Niels Abspoel <aboe@spectre.wraith.lan>
2019-02-17 22:15:39 +0100

    fix spec test

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper_acceptance'
 
 describe 'chrony class' do
-
   context 'default parameters' do
     # Using puppet_apply as a helper
     it 'works idempotently with no errors' do
