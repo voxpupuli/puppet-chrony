c57f664ebe61842f8171e0fcbe79b108019e7f34
Niels Abspoel <aboe76@gmail.com>
2019-08-05 22:18:33 +0200

    use litmus

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,28 +1,27 @@
 require 'spec_helper_acceptance'
 
 describe 'chrony class:' do
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'runs successfully' do
-      pp = "class { 'chrony': }"
-      # Run it twice and test for idempotency
-      apply_manifest(pp, catch_failures: true)
-      expect(apply_manifest(pp, catch_failures: true).exit_code).to be_zero
-    end
+  it 'with defaults' do
+		pp = <<-MANIFEST
+			class { 'chrony': }
+		MANIFEST
 
-    describe package('chrony') do
-      it { is_expected.to be_installed }
-    end
+		idempotent_apply(pp)
+	end
 
-    service = case fact('os.family')
-              when 'RedHat'
-                'chronyd'
-              else
-                'chrony'
-              end
-    describe service(service) do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
+
+  describe package('chrony') do
+    it { is_expected.to be_installed }
+  end
+
+  service = case fact('os.family')
+            when 'RedHat'
+              'chronyd'
+            else
+              'chrony'
+            end
+  describe service(service) do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 end
--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,28 +1,27 @@
 require 'spec_helper_acceptance'
 
 describe 'chrony class' do
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'works idempotently with no errors' do
-      pp = 'include chrony'
-      # Run it twice and test for idempotency
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
+  it 'with defaults' do
+    pp = <<-MANIFEST
+      class { 'chrony': }
+		MANIFEST
 
-    describe package('chrony') do
-      it { is_expected.to be_installed }
-    end
 
-    service = case fact('os.family')
-              when 'RedHat'
-                'chronyd'
-              else
-                'chrony'
-              end
-    describe service(service) do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
+		idempotent_apply(pp)
+  end
+
+	describe package('chrony') do
+  	it { is_expected.to be_installed }
+  end
+
+  service = case fact('os.family')
+            when 'RedHat'
+              'chronyd'
+            else
+              'chrony'
+            end
+  describe service(service) do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 end
