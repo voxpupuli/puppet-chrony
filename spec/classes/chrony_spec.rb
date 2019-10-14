bff524a4dddfc8bca020ded9b24d5750b1c6d97f
Przemek Anuszek <przemas@anuszek.net>
2019-10-14 14:50:54 +0200

    fact spec

--- spec/classes/chrony_spec.rb
+++ spec/classes/chrony_spec.rb
@@ -133,7 +133,7 @@ describe 'chrony' do
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_group('mrt') }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_replace(true) }
               it { is_expected.to contain_file('/etc/chrony/chrony.keys').with_content("0 sunny\n") }
-              if $::facts['os']['distro']['release']['major'] > '8' {
+              if facts[:os]['distro']['release']['major]'' > '8' {
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*leapsecmode slew$}) }
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*maxslewrate 1000$}) }
                 it { is_expected.to contain_file('/etc/chrony/chrony.conf').with_content(%r{^\s*smoothtime 400 0\.001 leaponly$}) }
