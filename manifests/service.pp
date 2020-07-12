34387c08a8f9c080eb02b2e4d07b8ba85e4228b6
Christer Ekholm <che@chrekh.se>
2020-07-12 20:54:39 +0200

    Remove unnecessary test on $service_ensure
    $service_ensure is required by init.pp to have type Stdlib::Ensure::Service, so there is
    no need for this test.

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,10 +7,6 @@ class chrony::service (
   $service_manage = $chrony::service_manage,
   $service_name   = $chrony::service_name,
 ) inherits chrony {
-  unless $service_ensure in ['running', 'stopped'] {
-    fail('service_ensure parameter must be running or stopped')
-  }
-
   if $service_manage {
     service { $service_name:
       ensure     => $service_ensure,
