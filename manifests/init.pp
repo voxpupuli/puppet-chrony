49671824cedbcc1a05be321578ee240d0299454f
Christian Hoffmann <mail@hoffmann-christian.info>
2021-07-12 10:58:54 +0200

    Add maxdistance

--- manifests/init.pp
+++ manifests/init.pp
@@ -174,6 +174,8 @@
 #   Configures how to insert the leap second mode.
 # @param leapsectz
 #   Specifies a timezone that chronyd can use to determine the offset between UTC and TAI.
+# @param maxdistance
+#   Sets the maximum root distance of a source to be acceptable for synchronisation of the clock.
 # @param maxslewrate
 #   Maximum rate for chronyd to slew the time. Only float type values possible, for example: `maxslewrate 1000.0`.
 # @param clientlog
@@ -242,6 +244,7 @@ class chrony (
   Optional[String] $smoothtime                                     = undef,
   Optional[Enum['system', 'step', 'slew', 'ignore']] $leapsecmode  = undef,
   Optional[String] $leapsectz                                      = undef,
+  Optional[Float] $maxdistance                                     = undef,
   Optional[Float] $maxslewrate                                     = undef,
   Optional[Float] $maxupdateskew                                   = undef,
   Optional[Numeric] $stratumweight                                 = undef,
