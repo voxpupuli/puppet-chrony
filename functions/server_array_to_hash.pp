9fc531157ef169cdc96101248b28e4f510052ccd
Tim Meusel <tim@bastelfreak.de>
2022-03-07 12:05:54 +0100

    server_array_to_hash: document return value

--- functions/server_array_to_hash.pp
+++ functions/server_array_to_hash.pp
@@ -1,6 +1,7 @@
 # @summary Function to normalise servers/pools/peers
 #
 # @api private
+# @return [Hash] returns the original hash or converts the array to an hash
 function chrony::server_array_to_hash(Variant[Hash,Array] $servers, $options = []) >> Hash {
   if $servers.is_a(Hash) {
     $servers
